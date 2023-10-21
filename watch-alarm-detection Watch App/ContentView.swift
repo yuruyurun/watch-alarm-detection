d1bcff34599daa09484b2c08f31637de96e5aec9
yuruyurun <yuruyuru-@outlook.jp>
2023-10-21 19:23:55 +0900

    Initial Commit

---
+++ watch-alarm-detection Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watch-alarm-detection Watch App
+//
+//  Created by Kyosuke Yurugi on 2023/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watch-alarm-detection Watch App/watch_alarm_detectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch_alarm_detectionApp.swift
+//  watch-alarm-detection Watch App
+//
+//  Created by Kyosuke Yurugi on 2023/10/21.
+//
+
+import SwiftUI
+
+@main
+struct watch_alarm_detection_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
