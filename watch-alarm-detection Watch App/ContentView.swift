582dff4759202634100af4c191ece7506de274ae
yuruyurun <62631585+yuruyurun@users.noreply.github.com>
2023-10-21 19:58:14 +0900

    test

--- watch-alarm-detection Watch App/ContentView.swift
+++ watch-alarm-detection Watch App/ContentView.swift
@@ -7,18 +7,72 @@
 
 import SwiftUI
 
+enum WarningViewType {
+    case warning1, warning2, none
+}
+
 struct ContentView: View {
+    @State private var started: Bool = false
+    @State private var currentDetectedSoundSource: String = "認識した音"
+    @StateObject var resultsObserver = AnalyzeService.shared.resultsObserver
+    
+    var buttonText: String {
+        if started {
+            return "Stop"
+        }
+        return "Start"
+    }
+    
+    var statusText: String {
+        if started {
+            return "Running"
+        }
+        return "Idle"
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("警報認識")
+                .font(.title)
+                .padding()
+            
+            Button(buttonText) {
+                started.toggle()
+                if started {
+                    AnalyzeService.shared.startAudioEngine()
+                } else {
+                    AnalyzeService.shared.stopAudioEngine()
+                    currentDetectedSoundSource = ""
+                }
+            }
+            .font(.body)
+            
+            Spacer()
+            
+            Text(self.currentDetectedSoundSource.capitalized.replacingOccurrences(of: "_", with: " ")).font(.body)
+            
+            Spacer()
         }
-        .padding()
+        .onAppear {
+            resultsObserver.detectedView = .none
+        }
+        .background(NavigationLink("", destination: WarningView1(), isActive: .init(get: { resultsObserver.detectedView == .warning1 }, set: { _ in })).opacity(0)) // この行を追加
+        .background(NavigationLink("", destination: WarningView2(), isActive: .init(get: { resultsObserver.detectedView == .warning2 }, set: { _ in })).opacity(0)) // この行を追加
+        .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name("ResultUpdated")))
+        { _ in
+            self.currentDetectedSoundSource = AnalyzeService.shared.currentItem
+            print("Update received in SwiftUI View")
+        }
     }
 }
 
+extension NSNotification {
+    static let ImageClick = Notification.Name.init("ImageClick")
+}
+
+
+//3.5と7秒や3と6秒などでやるロードのノイズなどを追加学習
 #Preview {
     ContentView()
 }
+//
--- watch-alarm-detection Watch App/watch_alarm_detectionApp.swift
+++ watch-alarm-detection Watch App/watch_alarm_detectionApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 @main
 struct watch_alarm_detection_Watch_AppApp: App {
+    init() {
+        // 通知の許可を要求
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if granted {
+                print("通知の許可が得られました")
+            } else {
+                print("通知の許可が拒否されました")
+            }
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
